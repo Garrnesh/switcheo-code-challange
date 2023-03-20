9c14922a9b0c7cbc3acf710dd53b772c25aabff6
Garrnesh <76935598+Garrnesh@users.noreply.github.com>
2023-03-20 18:55:49 +0800

    Create Problem_1.js

---
+++ Problem 1: Three ways to sum to n/Problem_1.js
@@ -0,0 +1,11 @@
+var sum_to_n_a = function(n) {
+    return (n+1)(n)/2 //Summation formula
+};
+
+var sum_to_n_b = function(n) {
+    // your code here
+};
+
+var sum_to_n_c = function(n) {
+    // your code here
+};
