7d804ca962a93630d1ecce899bc1453c386b31fa
Garrnesh <76935598+Garrnesh@users.noreply.github.com>
2023-03-20 19:16:56 +0800

    Update Problem_1.js

--- Problem 1: Three ways to sum to n/Problem_1.js
+++ Problem 1: Three ways to sum to n/Problem_1.js
@@ -1,11 +1,26 @@
 var sum_to_n_a = function(n) {
-    return (n+1)(n)/2 //Summation formula
+    //Take the sum of the first and last term and multiply by half the number of elements in the summation
+    
+    return (n+1)*(n)/2 
 };
 
 var sum_to_n_b = function(n) {
-    // your code here
+    //Brute force addition of all the terms using a for loop
+
+    let result = 0;
+    for(let i = 1; i<=n; i++){
+        result += i;
+    }
+    return result;
 };
 
 var sum_to_n_c = function(n) {
-    // your code here
+    // Similar to part b but using a while loop instead
+
+    let result = 0;
+    while(n){
+        result+=n;
+        n-=1;
+    }
+    return result;  
 };
