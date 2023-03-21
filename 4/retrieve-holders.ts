cdc5661adb694657f158a406d585184eb549ea89
Garrnesh <76935598+Garrnesh@users.noreply.github.com>
2023-03-21 20:06:27 +0800

    Completed question 1 and 4

---
+++ 4/retrieve-holders.ts
@@ -0,0 +1,23 @@
+const { ethers } = require('ethers');
+const ABI = require('./ABI.json');
+
+const switchToken: string = '0xc0ecb8499d8da2771abcbf4091db7f65158f1468';
+
+const holderAddress: string[] = [
+    '0xb5d4f343412dc8efb6ff599d790074d0f1e8d430',
+    '0x0020c5222a24e4a96b720c06b803fb8d34adc0af',
+    '0xd1d8b2aae2ebb2acf013b803bc3c24ca1303a392',
+];
+
+const provider = new ethers.JsonRpcProvider("https://bsc-dataseed.binance.org/")
+
+const contract = new ethers.Contract(switchToken, ABI, provider);
+
+holderAddress.forEach(async (add) => {
+    try{
+        var bal = await contract.balanceOf(add);
+        console.log(add, ethers.formatUnits(bal, 8));
+    }catch(err){
+        console.log(err);
+    }
+});
